4e152887386c1be56fe45cca61f2c7227d7575af
Ciurezu Bogdan (RBRO/PJ-DT) <cib2clj@bosch.com>
2021-05-27 14:00:09 +0300

    attempt 1

--- app/build/index.js
+++ app/build/index.js
@@ -17,6 +17,15 @@ app.get('/api/item/:itemId', (req, res) => {
     const { itemId } = req.params;
     res.json({ itemId });
 });
+app.post('/faceApi', (req, res) => {
+    res.json({ "mess": "got it" });
+});
+app.get('/faceApi', (req, res) => {
+    let img = '<img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600">';
+    console.log("entered");
+    console.log(req.body.uri);
+    res.send('<h1>SUP</h1>');
+});
 // start the express server
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
--- app/src/index.ts
+++ app/src/index.ts
@@ -16,6 +16,17 @@ app.get('/api/item/:itemId', (req, res) => {
   res.json({ itemId });
 });
 
+app.post('/faceApi', (req, res) => {
+  res.json({"mess": "got it"})
+});
+
+app.get('/faceApi', (req, res) => {
+  let img = '<img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600">'
+  console.log("entered")
+  console.log(req.body.uri)
+  res.send('<h1>SUP</h1>')
+});
+
 // start the express server
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
